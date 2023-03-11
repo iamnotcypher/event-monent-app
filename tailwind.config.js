f1b737f282c4a83ea44d034d0c0a86fd52b3679b
unknown <huzitaiwo@gmail.com>
2023-03-11 20:37:17 +0100

    decleared tailwindcss basestyles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: '#5271FF',
+        info: '#06C3B4',
+        danger: '#C34F06',
+        active: '#FFF5A7',
+      },
+      fontFamily: {
+        body: ['Poppins']
+      }
+    },
   },
   plugins: [],
 }

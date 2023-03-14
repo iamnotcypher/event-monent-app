65462c12d015db693b3c57dc8191e6a009cc8e9f
unknown <huzitaiwo@gmail.com>
2023-03-14 21:47:51 +0100

    fixing footer to bottom

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -9,9 +9,9 @@
       Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real. Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.<br/><br/>
       Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real. Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It’s not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.
     </p>
-    <div class="text-center mt-16 flex mb-24">
+    <div class="text-center my-16 mb-24 flex">
       <button class="btn bg-info mr-4">
-        <router-link to="/edit-event">Edit</router-link>
+        <router-link :to="{ name: 'EditEvent' }">Edit</router-link>
       </button>
       <button class="btn bg-danger">Delete</button>
     </div>

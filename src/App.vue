e1682a09b5b4d5c1dff8d838fac3ed11e34e2176
unknown <huzitaiwo@gmail.com>
2023-03-14 21:48:53 +0100

    fixing footer to bottom

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="font-body text-base relative">
+  <div class="font-body text-base flex flex-col relative min-h-screen">
     <router-view/>
     <Footer />
   </div>
 </template>
 
 <script>
-import Footer from './components/Footer'
-
+import Footer from "./components/Footer"
 export default {
-  name: 'App',
-  components: {
-    Footer,
-  }
+  name: "App",
+  components: { Footer, },
 }
 </script>

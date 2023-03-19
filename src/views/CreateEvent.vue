7876355f5d985a391fcdabff8a95df3f7a0fb8bd
unknown <huzitaiwo@gmail.com>
2023-03-19 01:14:16 +0100

    creating new moment

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -1,16 +1,17 @@
 <template>
-  <form class="max-w-md mt-8 mb-24 min-[100px]:px-4 md:px-16">
+  <form @submit.prevent="handleSubmit" class="max-w-md mt-8 mb-24 min-[100px]:px-4 md:px-16">
     <div class="mb-4">
+      <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam magnam natus sunt maxime ad commodi nisi culpa repellat explicabo corrupti minima laborum id debitis dolorem, ipsa tenetur velit porro dolorum est ratione aliquid sapiente! Incidunt quidem laboriosam facere blanditiis ad, enim porro explicabo unde fugit, eveniet dolorem consectetur quibusdam, minus qui vel. Dicta temporibus nulla eum sit ipsam ipsa?</p> -->
       <label class="text-sm block">Date in the future</label>
-      <input class="border w-full sm:w-96 rounded-md p-1" type="text">
+      <input class="border w-full sm:w-96 rounded-md p-1" type="text" v-model="futureDate">
     </div>
     <div class="mb-4">
       <label class="text-sm block">Title</label>
-      <input class="border w-full sm:w-96 rounded-md p-1" type="text">
+      <input class="border w-full sm:w-96 rounded-md p-1" type="text" v-model="title">
     </div>
     <div class="mb-4">
       <label class="text-sm block">Details</label>
-      <textarea class="border w-full sm:w-96 rounded-md p-1" name="" id="" cols="30" rows="8"></textarea>
+      <textarea class="border w-full sm:w-96 rounded-md p-1" name="" id="" cols="30" rows="8" v-model="details"></textarea>
     </div>
 
     <div class="text-center mt-8">
@@ -20,7 +21,33 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
+  name: 'CreateEvent',
+  data() {
+    return {
+      title: 'just another moment in time',
+      details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quaerat culpa reiciendis eveniet ut possimus necessitatibus ratione, esse voluptatem vitae deserunt assumenda earum, numquam iste, quibusdam ipsa ipsam expedita tempora ducimus. Quisquam excepturi possimus hic, vitae corrupti impedit quas vero.',
+      futureDate: '12-12-2055',
+      token: ''
+    }
+  },
+  methods: {
+    async handleSubmit() {
+
+      const response = await axios.post('moment', {
+        title: this.title,
+        details: this.details,
+        futureDate: this.futureDate
+      })
+
+      this.token = localStorage.getItem('token')
+      console.log(this.token)
+
+      console.log(response)
+
+    }
+  }
 }
 </script>
